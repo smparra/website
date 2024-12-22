da1bc772df3cb9566d9df07551eacbe0f8868c14
Your GitHub Username <your.email@example.com>
2024-12-21 23:28:39 -0400

    Fixed CV and Portfolio

--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -1,7 +1,6 @@
 import HeroSection from "../HeroSection";
 import MySkills from "../MySkills";
 import MyPortfolio from "../MyPortfolio";
-import ContactMe from "../ContactMe";
 import Footer from "../Footer";
 
 export default function Home() {
